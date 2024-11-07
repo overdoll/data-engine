de157544d35696e6041a091b0a6701b8a7e87708
ilyat <ilya.tkachov@hotmail.com>
2024-11-06 20:01:01 -0500

    update

---
+++ apps/backend/engine/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import hello_world
+
+router = DefaultRouter()
+
+urlpatterns = [
+    path("hello/", hello_world),
+]
