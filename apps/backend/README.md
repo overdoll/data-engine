7fda56d5c0a300eb03ac99f2663290239e327af5
Nikita <nikita.tkachov@protonmail.com>
2024-11-07 20:55:21 -0500

    fix

--- apps/backend/README.md
+++ apps/backend/README.md
@@ -1,17 +1,23 @@
 # install dependencies
-python -m venv venv
+
+```
+python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements-dev.txt
+```
 
 update .env in api/
 
 # run migrations
+
 python manage.py migrate
 
 # run server
+
 python manage.py runserver
 
 ---
 
 # create migrations
+
 python manage.py makemigrations
--- apps/backend/api/settings.py
+++ apps/backend/api/settings.py
@@ -45,10 +45,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "corsheaders",
     "engine",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -57,7 +59,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
 ROOT_URLCONF = "api.urls"
 
 TEMPLATES = [
@@ -135,3 +136,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+
+# Add CORS settings at the bottom of the file
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React default port
+    "http://127.0.0.1:3000",
+]
+
+# Optional: Allow credentials (cookies, authorization headers)
+CORS_ALLOW_CREDENTIALS = True
