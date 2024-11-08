22ab5eed2ff11be3ae48d375803d4489eb871a18
Nikita <nikita.tkachov@protonmail.com>
2024-11-07 19:39:09 -0500

    1

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/button"
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold mb-8">Welcome to the Dataset Manager</h1>
+      <Button>test</Button>
     </main>
   )
 }
--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -1,10 +1,74 @@
 import type { Config } from "tailwindcss"
-import medusaUi from "./src/ui-preset/plugin"
 import animate from "tailwindcss-animate"
+import { FONT_FAMILY_MONO } from "./src/ui-preset/constants"
+import { FONT_FAMILY_SANS } from "./src/ui-preset/constants"
+import { theme } from "./src/ui-preset/theme/extension/theme"
+import { typography } from "./src/ui-preset/theme/tokens/typography"
+import { colors } from "./src/ui-preset/theme/tokens/colors"
+import { effects } from "./src/ui-preset/theme/tokens/effects"
+import plugin from "tailwindcss/plugin"
+import { components } from "./src/ui-preset/theme/tokens/components"
 
 const config: Config = {
   content: ["./src/**/*.{ts,tsx,js,jsx}"],
   darkMode: ["class", '[data-mode="dark"]'],
-  plugins: [medusaUi, animate],
+  plugins: [
+    plugin(function medusaUi({ addBase, addComponents, config }) {
+      const [darkMode, className = ".dark"] = ([] as string[]).concat(config("darkMode", "media"))
+
+      addBase({
+        "*": {
+          borderColor: "var(--border-base)",
+        },
+      })
+
+      addComponents(typography)
+
+      addBase({
+        ":root": { ...colors.light, ...effects.light },
+        ...components.light,
+      })
+
+      if (darkMode === "class") {
+        addBase({
+          [className]: { ...colors.dark, ...effects.dark },
+        })
+      } else {
+        addBase({
+          "@media (prefers-color-scheme: dark)": {
+            ":root": { ...colors.dark, ...effects.dark },
+            ...components.dark,
+          },
+        })
+      }
+    }),
+    animate,
+  ],
+  theme: {
+    extend: {
+      ...theme.extend,
+      fontFamily: {
+        sans: FONT_FAMILY_SANS,
+        mono: FONT_FAMILY_MONO,
+      },
+      transitionProperty: {
+        fg: "color, background-color, border-color, box-shadow, opacity",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0px" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0px" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
 }
 export default config
