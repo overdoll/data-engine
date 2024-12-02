339aa6e46c06d9ce3e63eaab4da081712b73169a
Nikita <nikita.tkachov@protonmail.com>
2024-12-02 18:47:12 -0500

    1

--- apps/backend/api/settings.py
+++ apps/backend/api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import environ
+import sentry_sdk
 
 # Initialize environ
 env = environ.Env()
@@ -181,3 +182,17 @@ LOGGING = {
         },
     },
 }
+
+
+sentry_sdk.init(
+    dsn=env("SENTRY_DSN", default=""),
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    _experiments={
+        # Set continuous_profiling_auto_start to True
+        # to automatically start the profiler on when
+        # possible.
+        "continuous_profiling_auto_start": True,
+    },
+)
