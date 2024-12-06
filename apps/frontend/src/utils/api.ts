29ce66ce7e5dbad19cbe874b40c1e68b2115c756
ilyat <ilya.tkachov@hotmail.com>
2024-12-05 22:03:03 -0500

    update api url

--- apps/frontend/src/utils/api.ts
+++ apps/frontend/src/utils/api.ts
@@ -32,7 +32,7 @@ const useApiClient = () => {
     }
 
     return axios.create({
-      baseURL: "http://localhost:8000/api",
+      baseURL: process.env.NEXT_PUBLIC_API_URL,
       headers,
     })
   }
